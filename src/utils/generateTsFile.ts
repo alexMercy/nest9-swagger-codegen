9c61bcd410d2e786c27c21270a0cfec9bcbcd55e
alex <alexevansdance@gmail.com>
2024-08-26 17:00:24 +0300

    add prettier

--- src/utils/generateTsFile.ts
+++ src/utils/generateTsFile.ts
@@ -1,8 +1,33 @@
 import * as fs from 'fs-extra'
-import * as path from "path"
+import * as path from 'path'
+import * as prettier from 'prettier'
 
-export const generateTsFile = (rootPath: string, serviceName: string, type: string, data: string) => {
-  const servicePath = path.join(rootPath, `${serviceName}`)
-  fs.ensureDirSync(servicePath)
-  fs.writeFileSync(path.join(servicePath, `${serviceName}.${type}.ts`), data)
+const defaultPrettierOptions: prettier.Options = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+}
+
+export const generateTsFile = async (
+    rootPath: string,
+    serviceName: string,
+    type: string,
+    data: string
+) => {
+    const prettierConfigPath = await prettier.resolveConfigFile()
+    const prettierOptions = prettierConfigPath
+        ? await prettier.resolveConfig(prettierConfigPath)
+        : defaultPrettierOptions
+
+    const formattedContent = await prettier.format(data, {
+        ...prettierOptions,
+        parser: 'typescript',
+    })
+    const servicePath = path.join(rootPath, `${serviceName}`)
+    fs.ensureDirSync(servicePath)
+    fs.writeFileSync(
+        path.join(servicePath, `${serviceName}.${type}.ts`),
+        formattedContent
+    )
 }
